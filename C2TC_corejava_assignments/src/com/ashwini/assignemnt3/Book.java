865d8dc170c67f4becad1ea539b78df53bf7d076
Ashwini.E <Ashwini.E@LAPTOP-00S06NK2>
2025-08-19 21:20:39 +0530

    assignment3 completed

---
+++ C2TC_corejava_assignments/src/com/ashwini/assignemnt3/Book.java
@@ -0,0 +1,21 @@
+package com.ashwini.assignemnt3;
+
+public class Book extends Libraryitem {
+    private String author;
+
+    public Book(int id, String title, String author) {
+        super(id, title);
+        this.author = author;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book: [ID: " + getId() + ", Title: " + getTitle() + ", Author: " + author + "]");
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
+
+
---
+++ C2TC_corejava_assignments/src/com/ashwini/assignemnt3/Libraryitem.java
@@ -0,0 +1,26 @@
+package com.ashwini.assignemnt3;
+
+public class Libraryitem {
+    private int id;
+    private String title;
+
+    public Libraryitem(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public void displayInfo() {
+        System.out.println("ID: " + id + ", Title: " + title);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
+
+
